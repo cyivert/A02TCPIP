4da237f62c5df33c8447a1d318e728def44f1a45
Thannyyy <thanheuro20057@gmail.com>
2026-02-15 12:25:52 -0500

    Add client protocol commands and server config settings
    Added ProtocolCommands.cs with client command constants. Updated App.config with server IP and port settings.

---
+++ Client/ProtocolCommands.cs
@@ -0,0 +1,23 @@
+﻿/*
+* FILE            : ProtocolCommands.cs(Client)
+* PROJECT         : A02TCPIP
+* PROGRAMMER      :
+* FIRST VERSION   : 2026-02-15
+* DESCRIPTION     :
+*   Client-side protocol command names.
+*/
+
+namespace WordGameClient.Utils
+{
+    public static class ProtocolCommands
+    {
+        public const string LoginInfo = "login_info";
+        public const string WordValidation = "word_validation";
+        public const string TimeLeft = "time_left";
+        public const string ChoicesLeft = "choices_left";
+        public const string CheckEndGame = "check_end_game";
+        public const string CheckNewGame = "check_new_game";
+        public const string CheckForNewPlayer = "check_for_new_player";
+        public const string GetStringFromServer = "get_string_from_server";
+    }
+}
