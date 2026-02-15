f5cad828169089e21f28689c9a7d62f73b865957
cyivert <phcyiver2@gmail.com>
2026-02-15 12:07:09 -0500

    - Initialized listed protocol requirements written from the A02Report document to ProtocolCommands.cs

--- Server/Utils/ProtocolCommands.cs
+++ Server/Utils/ProtocolCommands.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/*
+* FILE : ProtocolCommands.cs
+* PROJECT : A02 TCPIP
+* PROGRAMMER : Cy Iver Torrefranca
+* DESCRIPTION :
+* Protocol command strings for client-server communication.
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +16,23 @@ namespace WordGameServer.Utils
 {
     internal class ProtocolCommands
     {
+        // Client to Server Commands
+        public const string Start = "START";
+        public const string Guess = "GUESS";
+        public const string Quit = "QUIT";
+        public const string EndGame = "ENDGAME";
+        public const string Progress = "PROGRESS";
+
+        // Server to Client Responses
+        public const string Welcome = "WELCOME";
+        public const string GameStart = "GAMESTART";
+        public const string WordFound = "WORD FOUND";
+        public const string WordDoesNotExist = "WORD DOES NOT EXIST";
+        public const string WordAlreadyExist = "WORD ALREADY EXIST";
+        public const string GameOver = "GAMEOVER";
+        public const string GameEnded = "GAMEENDED";
+        public const string ProgressUpdate = "PROGRESS";
+        public const string Goodbye = "GOODBYE";
+        public const string Error = "ERROR";
     }
 }
