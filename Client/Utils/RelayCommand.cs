2e34e38cc5f2b357802edeccdbf40d589ac39cef
Thannyyy <thanheuro20057@gmail.com>
2026-02-15 14:52:32 -0500

    feat: Add ViewModelBase and RelayCommand for MVVM pattern

---
+++ Client/Utils/RelayCommand.cs
@@ -0,0 +1,70 @@
+﻿/*
+* FILE            : RelayCommand.cs
+* PROJECT         : A02TCPIP
+* PROGRAMMER      : Tuan Thanh Nguyen
+* FIRST VERSION   : 2026-02-12
+* DESCRIPTION     :
+*   Basic ICommand implementation for WPF button bindings.
+*/
+
+using System;
+using System.Windows.Input;
+
+namespace WordGameClient.Utils
+{
+    public sealed class RelayCommand : ICommand
+    {
+        private readonly Action executeAction;
+        private readonly Func<bool>? canExecuteFunc;
+
+        public event EventHandler? CanExecuteChanged
+        {
+            add
+            {
+                CommandManager.RequerySuggested += value;
+
+                return;
+            }
+            remove
+            {
+                CommandManager.RequerySuggested -= value;
+
+                return;
+            }
+        }
+
+        public RelayCommand(Action executeAction, Func<bool>? canExecuteFunc)
+        {
+            this.executeAction = executeAction;
+            this.canExecuteFunc = canExecuteFunc;
+
+            return;
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            bool canExecute = true;
+
+            if (this.canExecuteFunc != null)
+            {
+                canExecute = this.canExecuteFunc.Invoke();
+            }
+
+            return (canExecute);
+        }
+
+        public void Execute(object? parameter)
+        {
+            this.executeAction.Invoke();
+
+            return;
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            CommandManager.InvalidateRequerySuggested();
+
+            return;
+        }
+    }
+}
---
+++ Client/ViewModels/ViewModelBase.cs
@@ -0,0 +1,31 @@
+﻿/*
+* FILE            : ViewModelBase.cs
+* PROJECT         : A02TCPIP
+* PROGRAMMER      : Tuan Thanh Nguyen
+* FIRST VERSION   : 2026-02-12
+* DESCRIPTION     :
+*   Base ViewModel implementing INotifyPropertyChanged. Providing property change notification for WPF data binding.
+*/
+
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WordGameClient.ViewModels
+{
+    public abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChangedEventHandler? handler = this.PropertyChanged;
+
+            if (handler != null)
+            {
+                handler.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+
+            return;
+        }
+    }
+}
